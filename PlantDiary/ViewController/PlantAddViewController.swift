b5e164fb956fe5f6cba89d788d633bc9fabc4f88
YuKyungKim <yukyungkim08@gmail.com>
2021-10-26 02:14:10 +0900

    이미지 선택 버튼 위치 수정, 이미지 선택 추가

--- PlantDiary/ViewController/PlantAddViewController.swift
+++ PlantDiary/ViewController/PlantAddViewController.swift
@@ -20,11 +20,19 @@ class PlantAddViewController: UIViewController {
     lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "icon_image")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 16
+        imageView.backgroundColor = UIColor(white: 0, alpha: 0.3)
         return imageView
     }()
     
     lazy var imageButton: UIButton = {
         let button = UIButton()
+        button.setTitle("이미지 선택", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 8
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
         return button
     }()
     
@@ -62,6 +70,8 @@ class PlantAddViewController: UIViewController {
         return textField
     }()
     
+    var imageUrl = ""
+    
     let disposeBag = DisposeBag()
     
     override func loadView() {
@@ -94,14 +104,16 @@ class PlantAddViewController: UIViewController {
             make.top.bottom.left.right.equalTo(self.imageAddView)
         }
         
-        self.imageAddView.addSubview(self.imageButton)
+        self.view.addSubview(self.imageButton)
         self.imageButton.snp.makeConstraints { (make) in
-            make.top.bottom.left.right.equalTo(self.imageAddView)
+            make.top.equalTo(self.imageAddView.snp.bottom).offset(8)
+            make.centerX.equalTo(self.view)
+            make.height.equalTo(32)
         }
         
         self.view.addSubview(self.nameView)
         self.nameView.snp.makeConstraints { (make) in
-            make.top.equalTo(self.imageAddView.snp.bottom).offset(16)
+            make.top.equalTo(self.imageButton.snp.bottom).offset(16)
             make.left.equalTo(self.view).offset(16)
             make.right.equalTo(self.view).offset(-16)
             make.height.equalTo(30)
@@ -143,11 +155,46 @@ class PlantAddViewController: UIViewController {
     }
     
     func rxBind() {
+        
         self.imageButton.rx.tap
-            .bind {
+            .bind { _ in
+                let alert = UIAlertController(title: "이미지 선택", message: nil, preferredStyle: .actionSheet)
                 
+                let camera = UIAlertAction(title: "카메라", style: .default) { (action) in
+                    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                        let imagePicker = UIImagePickerController()
+                        imagePicker.sourceType = .camera
+                        imagePicker.allowsEditing = true
+                        imagePicker.delegate = self
+                        self.present(imagePicker, animated: true, completion: nil)
+                    } else {
+                        // @TODO: 카메라 권한 요청
+                    }
+                }
+                
+                let gallery = UIAlertAction(title: "갤러리", style: .default) { (action) in
+                    if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                        let imagePicker = UIImagePickerController()
+                        imagePicker.sourceType = .photoLibrary
+                        imagePicker.allowsEditing = true
+                        imagePicker.delegate = self
+                        self.present(imagePicker, animated: true, completion: nil)
+                    } else {
+                        // @TODO: 갤러리 권한 요청
+                    }
+                }
+                
+                let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+                
+                alert.addAction(camera)
+                alert.addAction(gallery)
+                alert.addAction(cancel)
+                
+                self.present(alert, animated: true, completion: nil)
             }
-            .disposed(by: self.disposeBag)
+            .disposed(by: disposeBag)
+        
+        
     }
 
     /*
@@ -160,4 +207,17 @@ class PlantAddViewController: UIViewController {
     }
     */
 
+}
+
+extension PlantAddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            dismiss(animated: true) {
+                self.imageView.image = image
+                let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+                let imageUrl = documentsPath + "/" + "\(Date.timeIntervalSinceReferenceDate)"
+                self.imageUrl = imageUrl
+            }
+        }
+    }
 }
